19745a929f20af5269675d95709faf14c51e08bd
HcySunYang <HcySunYang@outlook.com>
2017-05-13 20:29:47 +0800

    Initialize the picker plugin

---
+++ src/plugins/picker/picker.js
@@ -0,0 +1,29 @@
+export default function (options) {
+
+    return function (Fmover) {
+        const {
+            Moved
+        } = Fmover
+        
+        return {
+            init (fmover) {
+                
+            },
+            start (fingerd) {
+                
+            },
+            move (fingerd) {
+                
+            },
+            end (fingerd) {
+                
+            },
+            cancel (fingerd) {
+                
+            }
+
+        }
+
+    }
+
+}
