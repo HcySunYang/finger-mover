5cbdff6e8d3b3ddb8cd334f112d6fc9a4618ebe9
HcySunYang <HcySunYang@outlook.com>
2017-05-18 11:54:11 +0800

    [build] 1.1.0

--- dist/finger-mover.js
+++ dist/finger-mover.js
@@ -1,5 +1,5 @@
 /*!
- * finger-mover.js v1.0.1
+ * finger-mover.js v1.0.2
  * (c) 2017 HcySunYang
  * Released under the MIT License.
  */
@@ -76,7 +76,6 @@ function is3DMatrix (el) {
     if (matrix === 'none') {
         return false
     }
-    console.log(matrix);
     var matrixArray = matrix.match(/[+-]?\d*[.]?\d+(?=,|\))/g);
     if (matrixArray.length > 6) {
         return true
--- src/plugins/simulation-scroll-y/dist/simulation-scroll-y.js
+++ src/plugins/simulation-scroll-y/dist/simulation-scroll-y.js
@@ -1,5 +1,5 @@
 /*!
- * simulation-scroll-y.js v1.0.1
+ * simulation-scroll-y.js v1.0.2
  * (c) 2017 HcySunYang
  * Released under the MIT License.
  */
