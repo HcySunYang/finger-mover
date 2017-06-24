b0bc90d1beb6ced63fd232776eea8f16922322c4
HcySunYang <HcySunYang@outlook.com>
2017-06-24 17:41:13 +0800

    devtool

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -49,6 +49,7 @@ let baseConfig = {
             }
         ]
     },
+    devtool: '#cheap-module-eval-source-map',
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': '"development"'
