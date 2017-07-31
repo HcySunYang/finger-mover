9d11fd566902c309b0b91967750f9833d95f5166
HcySunYang <HcySunYang@outlook.com>
2017-07-31 09:57:03 +0800

    [build] 1.1.9

--- dist/finger-mover.js
+++ dist/finger-mover.js
@@ -1,5 +1,5 @@
 /*!
- * finger-mover.js v1.1.7
+ * finger-mover.js v1.1.8
  * (c) 2017 HcySunYang
  * Released under the MIT License.
  */
--- src/plugins/fmover-slide-x/dist/fmover-slide-x.js
+++ src/plugins/fmover-slide-x/dist/fmover-slide-x.js
@@ -1,5 +1,5 @@
 /*!
- * fmover-slide-x.js v1.1.7
+ * fmover-slide-x.js v1.1.8
  * (c) 2017 HcySunYang
  * Released under the MIT License.
  */
